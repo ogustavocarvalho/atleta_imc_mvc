a348e67d6d2c91c2df18a9f9df90c1733cfac813
Gustavo Carvalho <gusenausa@gmail.com>
2026-05-03 19:45:33 -0300

    Adicionando validacão que remove os espacos extras de 'peso' e 'altura'

--- AtletaImcMVC/ViewController.swift
+++ AtletaImcMVC/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtAtleta: UITextField!
+    @IBOutlet weak var txtPeso: UITextField!
+    @IBOutlet weak var txtAltura: UITextField!
+    @IBOutlet weak var txtImc: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func calcular(_ sender: Any) {
+        //remove os espacos extras colocando o conteúdo entre duas constantes
+        let pesoTexto = txtPeso.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let alturaTexto = txtAltura.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let a = Atleta()
+        
+        a.nome = txtAtleta.text
+        a.peso = Float(txtPeso.text!)
+        a.altura = Float(txtAltura.text!)
+        a.calcular()
+        txtImc.text = String(format: "%.2f", a.imc!)
+    }
+    
 
 }
 
