070449198616c709403506a6b451bf6f8d7d9ef7
Gustavo Carvalho <gusenausa@gmail.com>
2026-05-03 18:53:25 -0300

    Initial Commit

---
+++ AtletaImcMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AtletaImcMVC
+//
+//  Created by Gustavo Carvalho on 03/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
